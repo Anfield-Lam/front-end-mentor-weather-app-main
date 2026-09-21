cc61e7f69951c07419dc221db63526c133eb372f
Anfield_Lam <1155258814@link.cuhk.edu.hk>
2026-09-22 00:53:22 +0800

    droplist with ticks

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,9 +3,11 @@ import "./Header.css"
 import logo from "./assets/images/logo.svg"
 import units from "./assets/images/icon-units.svg"
 import dropDownIcon from "./assets/images/icon-dropdown.svg"
+import checkmark from "./assets/images/icon-checkmark.svg"
 
 function Header() {
   const [active, setActive] = useState(false)
+  const [unit, setUnit] = useState(false) // false => metric, true => imperial
   return (
     <>
       <div className="header">
@@ -20,18 +22,18 @@ function Header() {
         {active && (
           <div className="dropDownList">
             <ol>
-              <li className="switch">Switch To Imperial</li>
+              <li className="switch" onClick={() => setUnit(!unit)}>Switch To {unit ? "Metric" : "Imperial"}</li>
               <li className="constant">Temperature</li>
-              <li>Celsius (°C)</li>
-              <li>Fahrenheit (°F)</li>
+              <li className={!unit && "selected"}>Celsius (°C) {!unit && <div className="checkmark-container"><img src={checkmark}/></div>}</li>
+              <li className={unit && "selected"}>Fahrenheit (°F) {unit && <div className="checkmark-container"><img src={checkmark}/></div>}</li>
               <div className="bottom-border"></div>
               <li className="constant">Wind Speed</li>
-              <li>km/h</li>
-              <li>mph</li>
+              <li className={!unit && "selected"}>km/h {!unit && <div className="checkmark-container"><img src={checkmark}/></div>}</li>
+              <li className={unit && "selected"}>mph {unit && <div className="checkmark-container"><img src={checkmark}/></div>}</li>
               <div className="bottom-border"></div>
               <li className="constant">Precipitation</li>
-              <li>Millimeters (mm)</li>
-              <li>Inches (in)</li>
+              <li className={!unit && "selected"}>Millimeters (mm) {!unit && <div className="checkmark-container"><img src={checkmark}/></div>}</li>
+              <li className={unit && "selected"}>Inches (in) {unit && <div className="checkmark-container"><img className="checkmark" src={checkmark}/></div>}</li>
             </ol>
           </div>
         )}
