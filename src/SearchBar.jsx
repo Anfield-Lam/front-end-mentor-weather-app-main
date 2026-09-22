8260c7abc9d95dc15448021fb3cae51885bd8c39
Anfield_Lam <1155258814@link.cuhk.edu.hk>
2026-09-22 20:34:19 +0800

    search bar have input

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import './SearchBar.css'
 import searchIcon from "./assets/images/icon-search.svg"
 
 function SearchBar() {
+  const [text, setText] = useState("");
+  const handleKeyDown = (event) => {
+    if (!/^[a-zA-Z]$/.test(event.key) && event.key !== "Backspace" && event.key !== " ") {
+      event.preventDefault();
+    }
+  }
   return (
     <>
-      <div className='SearchBarInput'><img className='searchIcon' src={searchIcon}/>Search for a place...</div>
+      <div className='SearchBarInput'>
+        <img className='searchIcon' src={searchIcon}/>
+        <input 
+          className='SearchBarText' 
+          type="text" 
+          value={text} 
+          onChange={(e) => setText(e.target.value)} 
+          onKeyDown={handleKeyDown} 
+          placeholder='Search for a place...'
+        />
+      </div>
       <div className='SearchBarButton'>Search</div>
     </>
   )
